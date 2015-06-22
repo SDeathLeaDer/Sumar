bbf8d70acf3d9655ace9e45f302ea9adfacbd9cf
SDeathLeaDer14 <wninachoquehuayta@gmail.com>
2015-06-22 14:58:14 -0500

    Initial commit

---
+++ app/src/main/java/model/Operacion.java
@@ -0,0 +1,15 @@
+package model;
+
+/**
+ * Created by wilder on 20/06/15.
+ */
+public class Operacion {
+
+    public Operacion(){
+
+    }
+
+    public int sumar(int number1, int number2 ){
+        return number1 + number2;
+    }
+}
